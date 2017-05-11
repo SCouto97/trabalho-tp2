712f9be109f6c4282b67022ef255bd7a9e5ee455
SCouto97 <samuelcoouto@hotmail.com>
2017-05-11 15:49:14 -0300

    Inserção na interface

--- TAD.h
+++ TAD.h
@@ -17,6 +17,51 @@ typedef struct celula_str {
 	apontador prox;
 }celula;
 
+
+
+typedef struct celula_str2 *apontador2;
+
+typedef struct {
+	char nomeDisciplina[25];
+	int idDisciplina;
+}tipoDisciplina;
+
+typedef struct celula_str2 {
+	tipoDisciplina disciplina;
+	apontador2 prox;	
+}celulaDisciplina;
+
+typedef struct {
+	apontador2 primeiro,ultimo;
+}tipoListaDisciplina;
+
+
+
+typedef struct celula_str3 *apontador3;
+
+typedef struct {
+	char nomeTopico[25];
+	int idTopico, idDisciplinaOrigem;
+}tipoTopico;
+
+typedef struct celula_str3 {
+	tipoTopico topico;
+	apontador3 prox;
+}celulaTopico;
+
+typedef struct {
+	apontador3 primeiro,ultimo;
+}tipoListaTopico;
+
+
+void CriaListaTopico(tipoListaTopico *lista);
+
+void InsereTopico(tipoListaTopico *lista, tipoTopico infosTopico);
+
+void ListarTopicos(int idDisciplina);
+
+void ListarDisciplinas();
+
 void CriaLista(tipoLista *lista);
 
 void InsereLista(tipoQuiz x, tipoLista *lista);
