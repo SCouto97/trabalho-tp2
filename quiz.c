6dfaf52e9c9ba0c7d7bbb4309a02afd5b798ec24
gabnunes <gabrielnunes_r@hotmail.com>
2017-05-12 12:17:47 -0300

    Add files via upload

--- quiz.c
+++ quiz.c
@@ -3,6 +3,32 @@
 #include "TAD.h"
 #include <string.h>
 
+/*Funcao responsavel por checar existencia da disciplina e topico recebida como parametro
+Retorna 1 se a disciplina e o topico inseridos existirem, 0 caso contrario*/
+int ValidarDisciplina(char *nomeDisciplina, char *nomeTopico) {
+	FILE *fp;	
+	char line[100]; /*Array de Char para armazenar linha do arquivo capturada*/
+	char *token;
+	if(fopen("disciplinas.txt","r") == NULL) {
+		printf("Essa Disciplina nao existe!");
+		return 0;
+	}
+	else {
+		fp = fopen("disciplinas.txt", "r");
+		while(fgets(line,90,fp) != NULL) {
+			if(strstr(line,nomeDisciplina) != NULL) {
+				token = strtok(line,"|");
+				token = strtok(NULL,"|");
+				token = strtok(NULL,"|");
+				if(strcmp(nomeTopico,token) == 0) {
+					return 1;
+				}
+			}
+		}
+		return 0;
+	}
+	fclose(fp);
+}
 
 
 void gerenciaPerguntas(char* nomearquivo, char* usuario_sessao){
@@ -11,6 +37,7 @@ void gerenciaPerguntas(char* nomearquivo, char* usuario_sessao){
     char pergunta[150], nomedisciplina[25], topico[25], resposta, line[100], *token;
     arq = fopen(nomearquivo,"r");
     int boolean = 0;
+    int boolean2 = 0;
     system("clear");
     printf("%%%%%%%%%%%%%%%%%%%%%%%%%%%% Sessao Ativa - %s %%%%%%%%%%%%%%%%%%%%%%%%%%%%\n", usuario_sessao);
     printf("Perfil: Administrador\n");
@@ -20,6 +47,17 @@ void gerenciaPerguntas(char* nomearquivo, char* usuario_sessao){
     scanf("%[^\n]s",nomedisciplina);
     printf("Digite o nome do topico: ");
     scanf(" %[^\n]s",topico);
+    while(boolean2 == 0) {
+	boolean2 = ValidarDisciplina(nomedisciplina, topico);
+	if(boolean2 == 0) {
+		printf("Disciplina e/ou Topico incorretos.\n");
+		getchar();
+		printf("Insira novamente a Disciplina: ");
+		scanf("%[^\n]s", nomedisciplina);
+		printf("Insira novamente o Topico: ");
+   		scanf(" %[^\n]s",topico);
+	}
+    }
     while(fgets(line,90,arq) != NULL) {
         if(strstr(line,topico) != NULL) {
             token = strtok(line,"|");
