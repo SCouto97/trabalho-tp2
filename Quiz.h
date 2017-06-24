2c86b6208228f7e08fc342cc6eac6884039c0d0b
SCouto97 <samuelcoouto@hotmail.com>
2017-06-23 21:54:17 -0300

    Comentários alterados

--- Quiz.h
+++ Quiz.h
@@ -25,14 +25,29 @@ typedef struct topicosId {
 	char topico[25];
 }topicosId;
 
+/*Funcao responavel pela criacao da lista de perguntas do quiz do topico passado como parametro.
+*Para isso, vare o arrquivo de perguntas, e, atraves da comparacao dos identificadores, 
+*filtra apenas as perguntas que fazem parte do topico que esta sendo exercitado e acrescenta na lista.
+*/
 void ListaPerguntas(char *nomearquivo, char *nometopico);
 
+/**Funcao Responsavel pela Impressao da Lista de Quiz e pela leitura das respostas do usuario.
+*Tambem informa ao usuario, ao termino do quiz, quais perguntas ele errou.
+*Recebe a Lista de Quiz e o nome do topico sobre o qual esta sendo realizado o quiz como parametro
+*/
 void ImprimirPerguntas(tipoLista lista, char *nometopico);
 
 void gerenciaPerguntas(char* nomearquivo, char* usuario_sessao);
 
+/**
+* Funcao para criar uma lista com cabeca com perguntas de um quiz
+*/
 void CriaLista(tipoLista *lista);
 
+
+/**
+*Funcao para inserir elemento(do tipoQuiz) em uma lista com cabeca
+*/
 void InsereLista(tipoQuiz x, tipoLista *lista);
 
 void DesalocarLista(tipoLista *lista);
