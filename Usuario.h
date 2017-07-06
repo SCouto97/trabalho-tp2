6827b34538644d90f9dca2280a29ad0e31442139
Gabriel <gabrielnunes_r@hotmail.com>
2017-07-06 19:49:20 -0300

    aaa

---
+++ Usuario.h
@@ -0,0 +1,26 @@
+#ifndef usuario_h_
+#define usuario_h_
+
+void TelaInicial();
+
+void LerDadosLogin();
+
+int TelaLogin(char *user, char *password, char *nomearquivo, int test);
+
+void TelaCadastro();
+
+void TelaRecuperarSenha();
+
+void TelaSaida();
+
+void TelaEntradaSistema(char *usuario_sessao);
+
+void TelaFinalizarSessao(char opcao);
+
+int ProcuraNomeUsuario(char *username, char *nomearquivo);
+
+void CadastrarDisciplina(char *usuario_sessao, char *nomearquivo);
+
+void listarDisciplinas(char* nomearquivo);
+
+#endif
