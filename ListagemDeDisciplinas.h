7d303d41d1e0fc59cc61b561101e3f46ffbece25
SCouto97 <samuelcoouto@hotmail.com>
2017-07-08 02:56:38 -0300

    Alterado

--- ListagemDeDisciplinas.h
+++ ListagemDeDisciplinas.h
@@ -28,14 +28,14 @@ void ListarMinhasDisciplinas(char* usuario_sessao);
 /**\brief Funcao para criar uma lista de disciplina com cabeca. 
 
 */
-void CriaListaDisciplina(tipoListaDisciplina *lista);
+int CriaListaDisciplina(tipoListaDisciplina *lista);
 
-void DesalocarListaDisciplina(tipoListaDisciplina *lista);
+int DesalocarListaDisciplina(tipoListaDisciplina *lista);
 
 
 /**\brief Funcao para inserir elemento(do tipoDisciplina) em uma lista com cabeca.
 
 */
-void InsereDisciplina(tipoListaDisciplina *lista, tipoDisciplina infosDisciplina);
+int InsereDisciplina(tipoListaDisciplina *lista, tipoDisciplina infosDisciplina);
 
 #endif
